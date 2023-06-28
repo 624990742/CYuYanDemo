2d3fc7cda7a1cbad6784b04798d2c3fae47d2341
xiaojia <>
2023-06-28 18:43:10 +0800

    up

--- cyuyanDemo/main.c
+++ cyuyanDemo/main.c
@@ -6,10 +6,114 @@
 //
 #include "JCDecimalConversion.h"
 #include "JCCarLicense.h"
-
+#include "stdio.h"
 int main(int argc, const char * argv[]) {
     // insert code here...
-    decimalConversion(200);
-    isCarNumberRightful(5,"NAB999");
+//    decimalConversion(200);
+//    isCarNumberRightful(5,"NAB999");
+//    int w=1,x=2,y=3,z=4;
+//    int result = w<x ? y<z ? y : z : w;
+//    printf("%d",result);
+  
+//    int i = 4,a[12]={1,4,7,10,2,5,8,11,3,6,9,12};
+//    printf("结果==>%d",a[a[i]]); 7
+
+    
+//     struct dent{
+//         int n;
+//         int *m;
+//     };
+//     int a=1,b=2,c=3;
+//     struct dent s[3]={{101,&b},{102,&b},{103,&c}},*p=s;
+//     printf("结果p++==>%d",*(p++)->m);
+//    printf("\n");
+//     printf("=========");
+//    printf("\n");
+//    printf("结果++p==>%d",*(++p)->m);
+//
+//    int  a,b;
+//    a=2;
+//    b=5;
+//    int c = a+b;
+//    int d = b++;
+//    printf("c==>%d,d==>%d",c,d);
+//
+   
+//    int a,b;
+//    int c = (a=2,b=5,a+b,b++);
+//    printf("\n");
+//    printf("c==>%d",c);
+//    printf("\n");
+//    printf("a==>%d",a);
+//    printf("\n");
+//    printf("a+b==>%d",a+b);
+//    printf("\n");
+//    printf("b++==>%d",b++);
+    
+//    int i;
+//    for (i = 0; i++ < 3;) {
+//        printf("%d\n",i);
+//    }
+    
+//    struct {
+//        int a;
+//        char b;
+//    }Q,*p=&Q;
+//
+//    printf("%d\n",(*p).b);
+//    printf("%d\n",*p.b);
+//    printf("%d\n",p->a);
+
+//
+//    char c = "A";int i=1,j;
+//    j=!c&&i++;
+//    printf("%d,%d",i,j);
+//
+//
+    
+//    int i = 0;
+//    for (i = -1; i< 0; i++) {
+//        printf("*");
+//        printf("\n");
+//    }
+//
+//
+//    int a = 25;
+//    int *ptr = &a;
+//    printf("%d",--*ptr);
+//
+
+//    int k=1,num=26;
+//    do{
+//        k*=num%100;
+//        num/=10;
+//    }while(num);
+//    printf("k==%d",k);
+//
+//    int i=10;
+//    switch (i) {
+//        case 9:i-=1;
+//        case 10:i-=1;
+//        case 11:i-=1;
+//        default:i-=1;
+//    }
+//    printf("i=%d",i);
+//
+//    int a=-2,b=0;
+//    while (++a&&b++)
+//   printf("a==%d,b==%d",a,b);
+    
+//    int x=1,y=10;
+//    do{
+//        y-=x;x++;
+//    }while(y-->0);
+//    printf("x==>%d",x);
+    
+    int i=10,j=2;
+    i*=j+8;
+    printf("%d",i);
+    
+
+    
     return 0;
 }
